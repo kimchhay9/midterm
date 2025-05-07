b161ce9d0124d17e99be757712439aee2d19788b
kimchhay9 <161797013+kimchhay9@users.noreply.github.com>
2025-05-07 15:51:22 +0700

    Exam

---
+++ src/main/java/GIC4C/demo/SoeungKimchhayApplication.java
@@ -0,0 +1,13 @@
+package GIC4C.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoeungKimchhayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoeungKimchhayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GIC4C/demo/SoeungKimchhayApplicationTests.java
@@ -0,0 +1,13 @@
+package GIC4C.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoeungKimchhayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
